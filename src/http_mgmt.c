127e91e3614f2451b9763dfe88bb4c7b7188bc2a
Lao Ke <36225368+kehengzhong@users.noreply.github.com>
2021-06-22 23:04:32 +0800

    Add files via upload

--- src/http_mgmt.c
+++ src/http_mgmt.c
@@ -26,8 +26,8 @@
 #include "http_cache.h"
 #include "http_script.h"
 
-char * g_http_version = "1.2.4";
-char * g_http_build = "eJet/1.2.4 Web Server built "__DATE__" "__TIME__" "
+char * g_http_version = "1.2.6";
+char * g_http_build = "eJet/1.2.6 Web Server built "__DATE__" "__TIME__" "
                       "by kehengzhong@hotmail.com";
 char * g_http_author = "Lao Ke <kehengzhong@hotmail.com>";
 
